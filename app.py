6b7869fcb38556694895dcbf0ce9f1b6ef5509b7
JJY01215 <jyj954533@gmail.com>
2025-06-16 18:53:25 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, f1_score
+
+# 題目一
+df = pd.read_csv('pokemon.csv')
+
+# 題目二 
+df = df.dropna(subset=['Attack', 'Defense'])
+
+# 題目三
+scaler = StandardScaler()
+
+# 題目四
+df = df[df['Type1'].isin(['Normal', 'Fighting', 'Ghost'])]
+X = scaler.fit_transform(df[['Attack', 'Defense']]) 
+y = df['Type1']
+
+clf = SVC()
+clf.fit(X, y)
+
+y_pred = clf.predict(X)
+
+# 題目五
+accuracy = accuracy_score(y, y_pred)
+f1 = f1_score(y, y_pred, average='weighted')
+# 題目六
+new= [[100, 75]]
+new_data_scaled = scaler.transform(new)
+prediction = clf.predict(new_data_scaled)
+
+# 答案一
+wrong = (y_pred != y).sum()
+print(f"錯誤分類個數：{wrong}")
+
+# 答案二
+print(f"準確度 Accuracy：{accuracy:.4f}")
+
+# 答案三
+print(f"F1-score：{f1:.4f}")
+
+# 答案四
+print(f"寶可夢預測為：{prediction[0]}")
